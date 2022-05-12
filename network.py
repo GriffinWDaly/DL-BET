ebeedf8c92a357dfd703d3054cc25681b06781ac
GriffinWDaly <76232304+GriffinWDaly@users.noreply.github.com>
2022-05-12 00:48:35 -0400

    Update network.py

--- network.py
+++ network.py
@@ -805,3 +805,114 @@ class ResAttU_Net3D(nn.Module):
         d1 = self.Conv_1x1(d2)
 
         return d1
+
+class Recurrent_block_3D(nn.Module):
+  def __init__(self, ch_out,t=2):
+    super(Recurrent_block_3D,self).__init__()
+    self.t = t
+    self.ch_out = ch_out
+    self.conv = nn.Sequential(
+        nn.Conv3d(ch_out,ch_out,kernel_size = 3, stride = 1, padding = 1, bias = True),
+        nn.BatchNorm3d(ch_out),
+        nn.ReLu(inplace=True)
+    )
+  def forward(self,x):
+    for i in range(self.t):
+      if i==0:
+        x1 = self.conv(x)
+
+      x1 = self.conv(x+x1)
+    return x1
+
+class RRCNN_block_3D(nn.Module):
+    def __init__(self, ch_in, ch_out, t=2):
+        super(RRCNN_block_3D,self).__init__()
+        self.RRCNN = nn.Sequential(
+          Recurrent_block_3D(ch_out,t=t),
+          Recurrent_block_3D(ch_out,t=t)
+        )
+        self.Conv_1x1 = nn.Conv3d(ch_in,ch_out, kernel_size=1,stride=1,padding=0)
+
+    def forward(self,x):
+      x = self.Conv_1x1(x)
+      x1 = self.RRCNN(x)
+      return x+x1
+
+
+class R2AttU_Net3D(nn.Module):
+  def __init__(self,img_ch=2, output_ch=1, t=2):
+    super(R2AttU_Net,self).__init__()
+
+    self.Maxpool = nn.MaxPool3d(kernel_stride=2,stride=2)
+    self.Upsample = nn.Upsample(scale_factor=2)
+
+    self.RRCNN1 = RRCNN_block_3D(ch_in=img_ch,ch_out=64,t=t)
+
+    self.RRCNN2 = RRCNN_block_3D(ch_in=64,ch_out=128,t=t)
+
+    self.RRCNN3 = RRCNN_block_3D(ch_in=128,ch_out=256,t=t)
+
+    self.RRCNN4 = RRCNN_block_3D(ch_in=256,ch_out=512,t=t)
+
+    self.RRCNN5 = RRCNN_block_3D(ch_in=512,ch_out=1024,t=t)
+
+    self.Up5 = up_conv_3D(ch_in=1024,ch_out=512)
+    self.Att5 = Attention_block_3D(F_g=512,F_l=512,F_int=256)
+    self.Up_RRCNN5 = RRCNN_block_3D(ch_in=1024, ch_out=512,t=t)
+    
+    self.Up4 = up_conv_3D(ch_in=512,ch_out=256)
+    self.Att4 = Attention_block_3D(F_g=256,F_l=256,F_int=128)
+    self.Up_RRCNN4 = RRCNN_block_3D(ch_in=512, ch_out=256,t=t)
+    
+    self.Up3 = up_conv_3D(ch_in=256,ch_out=128)
+    self.Att3 = Attention_block_3D(F_g=128,F_l=128,F_int=64)
+    self.Up_RRCNN3 = RRCNN_block_3D(ch_in=256, ch_out=128,t=t)
+    
+    self.Up2 = up_conv_3D(ch_in=128,ch_out=64)
+    self.Att2 = Attention_block_3D(F_g=64,F_l=64,F_int=32)
+    self.Up_RRCNN2 = RRCNN_block_3D(ch_in=128, ch_out=64,t=t)
+
+
+    self.Conv_1x1 = nn.Conv3d(64,output_ch,kernel_size=1,stride=1,padding=0)
+
+    def forward(self,x):
+        # encoding path
+        x1 = self.RRCNN1(x)
+
+        x2 = self.Maxpool(x1)
+        x2 = self.RRCNN2(x2)
+        
+        x3 = self.Maxpool(x2)
+        x3 = self.RRCNN3(x3)
+
+        x4 = self.Maxpool(x3)
+        x4 = self.RRCNN4(x4)
+
+        x5 = self.Maxpool(x4)
+        x5 = self.RRCNN5(x5)
+
+        # decoding + concat path
+        d5 = self.Up5(x5)
+        x4 = self.Att5(g=d5,x=x4)
+        d5 = torch.cat((x4,d5),dim=1)
+        d5 = self.Up_RRCNN5(d5)
+        
+        d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4,x=x3)
+        d4 = torch.cat((x3,d4),dim=1)
+        d4 = self.Up_RRCNN4(d4)
+
+        d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3,x=x2)
+        d3 = torch.cat((x2,d3),dim=1)
+        d3 = self.Up_RRCNN3(d3)
+
+        d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2,x=x1)
+        d2 = torch.cat((x1,d2),dim=1)
+        d2 = self.Up_RRCNN2(d2)
+
+        d1 = self.Conv_1x1(d2)
+        
+        return d1
+
